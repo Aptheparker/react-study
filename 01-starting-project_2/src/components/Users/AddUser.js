2b08e97ab5c76f396c025c4940685bcd8bd4bedb
aptheparker <aptheparker@gmail.com>
2023-03-12 20:31:49 +0100

    3rd project AddUser.js

--- 01-starting-project_2/src/components/Users/AddUser.js
+++ 01-starting-project_2/src/components/Users/AddUser.js
@@ -1,17 +1,25 @@
+// components
+import Card from "../UI/Card";
+
+// css
+import classes from "./AddUser.module.css";
+
 const AddUser = () => {
   const addUserHandler = (event) => {
     event.preventDefault();
   };
-  
+
   return (
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
-}
+};
 
 export default AddUser;
