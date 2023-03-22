8ca4da90c94c1985b3a287d5b0c73e185d85bdc9
aptheparker <aptheparker@gmail.com>
2023-03-22 03:33:15 +0100

    validation added

--- add-user/src/App.js
+++ add-user/src/App.js
@@ -1,11 +1,26 @@
-// components
-import AddUser from './components/Users/AddUser';
+// hooks
+import { useState } from "react";
 
+// components
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: uName, age: uAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList} />
     </div>
   );
 }
---
+++ add-user/src/components/Users/UsersList.js
@@ -0,0 +1,21 @@
+// components
+import Card from "../UI/Card";
+
+// css
+import classes from "./UsersList.module.css";
+
+const UsersList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.name} ({user.age} years old)
+          </li>
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default UsersList;
