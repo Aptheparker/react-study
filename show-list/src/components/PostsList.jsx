6b777579d7fa4efcd82ad665fd11e023308110c3
aptheparker <aptheparker@gmail.com>
2023-07-17 17:21:48 +0900

    event listener added

--- show-list/src/components/PostsList.jsx
+++ show-list/src/components/PostsList.jsx
@@ -1,14 +1,18 @@
-import Post from './Post';
+import Post from "./Post";
+import NewPost from "./NewPost";
 
-import classes from './PostsList.module.css';
+import classes from "./PostsList.module.css";
 
-function PostsList(){
-  return(
-    <ul className={classes.posts}>
-      <Post author="ap" body="React.js is awesome!" />
-      <Post author="sp" body="Check out the full course!"/>
-    </ul>
-  )
+function PostsList() {
+  return (
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="ap" body="React.js is awesome!" />
+        <Post author="sp" body="Check out the full course!" />
+      </ul>
+    </>
+  );
 }
 
 export default PostsList;
