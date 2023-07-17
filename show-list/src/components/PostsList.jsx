a42c1efa9bbb9096057e93272bdfc87ed75a1c2c
aptheparker <aptheparker@gmail.com>
2023-07-17 17:41:28 +0900

    state lifted for PostsList

--- show-list/src/components/PostsList.jsx
+++ show-list/src/components/PostsList.jsx
@@ -1,14 +1,27 @@
+import {useState} from "react";
+
 import Post from "./Post";
 import NewPost from "./NewPost";
 
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost onBodyChange={changeBodyHandler} onAuthorChange={changeAuthorHandler}/>
       <ul className={classes.posts}>
-        <Post author="ap" body="React.js is awesome!" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="sp" body="Check out the full course!" />
       </ul>
     </>
