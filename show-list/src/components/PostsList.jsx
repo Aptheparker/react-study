e305c51d04d90cc1557e980f5170e03d8682fe29
aptheparker <aptheparker@gmail.com>
2023-07-17 20:35:40 +0900

    modal backdrop added

--- show-list/src/components/PostsList.jsx
+++ show-list/src/components/PostsList.jsx
@@ -7,8 +7,13 @@ import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
 function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
 
   function changeBodyHandler(event) {
     setEnteredBody(event.target.value);
@@ -20,12 +25,14 @@ function PostsList() {
 
   return (
     <>
-      <Modal>
-        <NewPost
-          onBodyChange={changeBodyHandler}
-          onAuthorChange={changeAuthorHandler}
-        />
-      </Modal>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={changeBodyHandler}
+            onAuthorChange={changeAuthorHandler}
+          />
+        </Modal>
+      )}
       <ul className={classes.posts}>
         <Post author={enteredAuthor} body={enteredBody} />
         <Post author="sp" body="Check out the full course!" />
