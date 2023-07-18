4e2d52843d5534ba75385895987e6dbc2717ee8b
aptheparker <aptheparker@gmail.com>
2023-07-18 09:07:58 +0900

    form submit added

--- show-list/src/components/NewPost.jsx
+++ show-list/src/components/NewPost.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
+
 import classes from "./NewPost.module.css";
 
-function NewPost({onBodyChange, onAuthorChange, onCancel, onSubmit}) {
+function NewPost({ onCancel }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+  
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+    const post = {
+      body: enteredBody,
+      name: enteredAuthor,
+    };
+    console.log(post);
+    onCancel();
+  }
 
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onBodyChange} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onAuthorChange}/>
+        <input type="text" id="name" required onChange={changeAuthorHandler} />
       </p>
       <p className={classes.actions}>
-        <button type="button" onClick={onCancel}>Cancel</button>
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
         <button type="submit">Submit</button>
       </p>
     </form>
--- show-list/src/components/PostsList.jsx
+++ show-list/src/components/PostsList.jsx
@@ -1,36 +1,19 @@
-import { useState } from "react";
-
 import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 import classes from "./PostsList.module.css";
 
-function PostsList({isPosting, onStopPosting}) {
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function changeAuthorHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+function PostsList({ isPosting, onStopPosting }) {
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={changeBodyHandler}
-            onAuthorChange={changeAuthorHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="sp" body="Check out the full course!" />
       </ul>
     </>
